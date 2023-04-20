8e81125f93f20a71776a62c8ba984e6101b44015
12jossy <wakariawakinyua@gmail.com>
2023-04-20 09:51:21 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
 include<stdio.h>
 #include<unistd.h>
-
 /**
- * main - Entry point
- *
- * Description:print a quote using write function
- * write(int fd.const void *buf.size_t count)
- *
- * Return: 1 (not success)
+ * main - A c program that prints a line to the standard error
+ * Return 1(success)
  */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" -Dora korpar, 2015-10-19\n";
-	write(1,quo,$9);
-		return(1);
+	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n",stdout);
+	return(1);
 }

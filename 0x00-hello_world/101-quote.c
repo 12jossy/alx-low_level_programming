ad0388924469aa1db2bd10b13bc3eb91d66adb3d
12jossy <wakariawakinyua@gmail.com>
2023-04-20 15:10:03 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include<unistd.h>
 /**
  * main - Entry
- * Return: Always 1(success)
+ * Return: 1
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful \* -Dora korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
 return (1);
 }
